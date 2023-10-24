6c1582fbc4e6642563c4c2189daf7fb183735f02
Adal Díaz Fariña <diazfarina.adal@gmail.com>
2023-10-24 13:05:12 +0100

    Build de la aplicacion

---
+++ src/app/gifs/gifs.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs',
+  templateUrl: './gifs.component.html',
+})
+export class GifsComponent {
+
+}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from 'src/app/gifs/servicios/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor(private gifsService: GifsService) {}
+
+  get tagsHistory(): string[] {
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string): void {
+    this.gifsService.searchTag(tag);
+  }
+}
